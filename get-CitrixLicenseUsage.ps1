93f394bd3c1afb18c84a57636771ae90cb836b6c
Cactacae <4174006+cactacae@users.noreply.github.com>
2017-10-26 10:30:15 -0700

    Change icm to Invoke-Command for clarity

--- get-CitrixLicenseUsage.ps1
+++ get-CitrixLicenseUsage.ps1
@@ -1,6 +1,6 @@
 # Get-CitrixLicensing
 Param($LicenseServer)
-$licensePool = icm -ComputerName $LicenseServer -ScriptBlock {get-ciminstance -class "Citrix_GT_License_Pool" -Namespace "ROOT\CitrixLicensing"}
+$licensePool = Invoke-Command -ComputerName $LicenseServer -ScriptBlock {get-ciminstance -class "Citrix_GT_License_Pool" -Namespace "ROOT\CitrixLicensing"}
 $licensePool | Select-Object @{n="Product";e={$_.PLD}},
                             @{n="Model";e={"Server"}},  
                             @{n="Type";e={$_.LicenseType}},
