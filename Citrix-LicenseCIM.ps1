7c76944a14656e83c06f12312bbd8505aa55182f
Cactacae <4174006+cactacae@users.noreply.github.com>
2017-10-26 10:22:13 -0700

    Citrix License Report via CIM

---
+++ Citrix-LicenseCIM.ps1
@@ -0,0 +1,10 @@
+# Get-CitrixLicensing
+Param($LicenseServer)
+$licensePool = icm -ComputerName $LicenseServer -ScriptBlock {get-ciminstance -class "Citrix_GT_License_Pool" -Namespace "ROOT\CitrixLicensing"}
+$licensePool | Select-Object @{n="Product";e={$_.PLD}},
+                            @{n="Model";e={"Server"}},  
+                            @{n="Type";e={$_.LicenseType}},
+                            @{n="Installed";e={$_.Count}},
+                            @{n="In Use";e={$_.InUseCount}},
+                            @{n="Available";e={$_.PooledAvailable}},
+                            @{n="% in use";e={($_.InUseCount/$_.Count)*100}}
